97e0f74e91ed48191b7c78de942b1168fca9a871
Никита Петров <n.petrov@yclients.tech>
2024-03-16 01:03:12 +0300

    add env

---
+++ ci/build-env.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cd src
+mkdir -p environments
+cd environments
+
+echo "export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: '"$POMIDARO_FIREBASE_API_KEY"',
+    authDomain: '"$POMIDARO_FIREBASE_AUTH_DOMAIN"',
+    projectId: '"$POMIDARO_FIREBASE_PROJECT_ID"',
+    storageBucket: '"$POMIDARO_FIREBASE_STORAGE_BUCKET"',
+    messagingSenderId: '"$POMIDARO_FIREBASE_MESSAGING_SENDER_ID"',
+    appId: '"$POMIDARO_FIREBASE_APP_ID"',
+  }
+};" > environment.ts
+
+echo 'Finished setting up environment'
+
+exit 0
