2f8fe73e4f96baa55606975ecedad89383a70585
webnikler <webnikler@gmail.com>
2024-04-04 22:54:25 +0300

    конфликты

--- src/app/views/home/screens/session/session.routes.ts
+++ src/app/views/home/screens/session/session.routes.ts
@@ -2,13 +2,8 @@ import { Routes } from '@angular/router';
 import { sessionTableResolver } from '../../resolvers/session-table.resolver';
 import { sessionResolver } from '../../resolvers/session.resolver';
 import { SessionViewComponent } from './session.component';
-<<<<<<< HEAD:src/app/views/home/screens/session/session.routes.ts
 import { SessionTableViewComponent } from './table/session-table.component';
 import { SessionSettingsComponent } from './settings/settings.component';
-=======
-import { SessionTableViewComponent } from './views/table/session-table.component';
-import { SessionSettingsComponent } from './views/settings/settings.component';
->>>>>>> bb1eeb81d164267f233a26cb812c555afedde278:src/app/views/home/views/session/session.routes.ts
 import { createSessionResolver } from '../../resolvers/create-session.resolver';
 
 export const SESSION_ROUTES: Routes = [
